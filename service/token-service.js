3c9f5dd7a447d9fcbd9e6611978843405fc4f707
Timofey <mr.t09@mail.ru>
2022-06-11 18:58:43 +0300

    update

--- service/token-service.js
+++ service/token-service.js
@@ -5,7 +5,7 @@ const config = require("config");
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, config.get("jwtSecret"), {
-      expiresIn: "30m",
+      expiresIn: "30s",
     });
     const refreshToken = jwt.sign(payload, config.get("jwtRefreshSecret"), {
       expiresIn: "30d",
